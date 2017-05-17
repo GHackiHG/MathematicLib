3973e71c2f6c536f7e594e2b76faf8553fc8798a
Maxim Rudometov <makc1901@yandex.ru>
2017-05-17 17:58:44 +0300

    some fixes

--- MathematicLib/MathematicLib/IMathResult.cs
+++ MathematicLib/MathematicLib/IMathResult.cs
@@ -1,7 +1,15 @@
 ﻿namespace MathematicLib
 {
+    /// <summary>
+    /// Интерфейс для вычислений
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IMathResult<out T>
     {
+        /// <summary>
+        /// Возвращает результат вычислений
+        /// </summary>
+        /// <returns></returns>
         T GetResult();
     }
 }
