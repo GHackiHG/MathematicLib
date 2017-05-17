defcdebe5e62b09a75d575937424f7990af0f47a
Maxim Rudometov <makc1901@yandex.ru>
2017-05-17 16:21:45 +0300

    Comments

--- MathematicLib/MathematicLib/Interpolation/LagrangesIngterpolationResults.cs
+++ MathematicLib/MathematicLib/Interpolation/LagrangesIngterpolationResults.cs
@@ -2,9 +2,18 @@
 
 namespace MathematicLib.Interpolation
 {
+    /// <summary>
+    /// Результаты вычисления полинома Лагранжа
+    /// </summary>
     public class LagrangesIngterpolationResults
     {
+        /// <summary>
+        /// Таблица значений
+        /// </summary>
         public List<List<string>> Table { get; set; }
+        /// <summary>
+        /// NotSimpled - полином, Simplied - полином, где приведены подобные
+        /// </summary>
         public (string NotSimpled,string Simplied) Polinoms { get; set; }
     }
 }
