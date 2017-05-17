ab20198e4055a9d3a4079a0ec69e31ad6a004893
Maxim Rudometov <makc1901@yandex.ru>
2017-05-17 17:53:06 +0300

    Added Trapezoid Integral

---
+++ MathematicLib/MathematicLib/Integration/TrapezoidIntegration.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathematicLib.Integration
+{
+    /// <summary>
+    /// Нахождение определенного интеграла методом трапеций
+    /// </summary>
+    public class TrapezoidIntegration : IMathResult<IntegrationResults>
+    {
+        FormulaInvoker Invoker;
+        private double a;
+        private double b;
+        private int n;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="expression">Подинтегральное выражение</param>
+        /// <param name="_a">Начало отрезка</param>
+        /// <param name="_b">Конкц отрезка</param>
+        /// <param name="_n">Количество интервалов</param>
+        public TrapezoidIntegration(string expression, double _a, double _b, int _n)
+        {
+            if (_n <= 0)
+                throw new ArgumentException("N can't be equal to 0 or less then 0");
+
+            if (string.IsNullOrWhiteSpace(expression))
+                throw new ArgumentNullException("expression");
+
+            Invoker = new FormulaInvoker(expression);
+            if (_a < _b)
+            {
+                a = _a;
+                b = _b;
+            }
+            else
+            {
+                a = _b;
+                b = _a;
+            }
+            n = _n;
+        }
+        /// <summary>
+        /// Вовращает результат вычисления и набор пар - (X;Y)
+        /// </summary>
+        /// <returns></returns>
+        public IntegrationResults GetResult()
+        {
+            double h = (b - a) / n, s = 0, TEMP = a;
+            double[] x = new double[n + 1];
+            List<(double X, double Y)> Pairs = new List<(double, double)>();
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = TEMP;
+                if ((x[i] == a) || (x[i] == b))
+                {
+                    if (double.TryParse(Invoker.GetResult(x[i]), out double sum4))
+                    {
+                        s = s + 1 / 2 * sum4;
+                        Pairs.Add((X: x[i], Y: 1 / 2 * sum4));
+                    }
+                    else
+                    {
+                        return new IntegrationResults { Result = 0, Values = new List<(double X, double Y)>() };
+                    }
+                }
+                else
+                {
+                    if (double.TryParse(Invoker.GetResult(x[i]), out double sum))
+                    {
+                        s = s + sum;
+                        Pairs.Add((X: x[i], Y: sum));
+                    }
+                    else
+                    {
+                        return new IntegrationResults { Result = 0, Values = new List<(double X, double Y)>() };
+                    }
+                }
+                TEMP = TEMP + h;
+            }
+            return new IntegrationResults { Result = (h * s), Values = Pairs };
+        }
+    }
+}
